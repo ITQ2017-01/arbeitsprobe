aea6f61d44fe0bb2212d7afbb8868d0d75015154
IFQ2017-01 <ifq2017-01@cloud.bs-hassfurt.de>
2019-07-19 11:55:23 +0200

    x

--- Component.js
+++ Component.js
@@ -13,11 +13,9 @@ sap.ui.define([
 		},
 
 		init : function () {
-			// call the init function of the parent
 			UIComponent.prototype.init.apply(this, arguments);
 
-			// set data model
-			var oData = {
+            var oData = {
 				recipient : {
 					name : "World"
 				}
