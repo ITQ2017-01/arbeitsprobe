1e3bfd31c30524cd9fbe689cec5b1f18fac3b171
IFQ2017-01 <ifq2017-01@cloud.bs-hassfurt.de>
2019-07-19 11:35:47 +0200

    x

--- controller/StudentPanel.js
+++ controller/StudentPanel.js
@@ -1,15 +1,8 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageToast"
-], function (Controller, MessageToast) {
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
 	"use strict";
 	return Controller.extend("hts.it.fiae.hts.controller.StudentPanel", {
-		onShowHello : function () {
-			var oBundle = this.getView().getModel("i18n").getResourceBundle();
-			var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-			var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-			MessageToast.show(sMsg);
-		}
 	});
 
 });
