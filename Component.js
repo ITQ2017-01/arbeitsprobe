e89f8a94b4cab12ab51ce9ed079404e6abc10676
IFQ2017-01 <ifq2017-01@cloud.bs-hassfurt.de>
2019-07-19 11:26:46 +0200

    x

---
+++ Component.js
@@ -0,0 +1,37 @@
+sap.ui.define([
+	"sap/ui/core/UIComponent",
+	"sap/ui/model/json/JSONModel",
+	"sap/m/MessageToast",
+    "sap/m/MessageBox"
+], function (UIComponent, JSONModel, MessageToast, MessageBox) {
+	"use strict";
+
+	return UIComponent.extend("hts.it.fiae.itq.Component", {
+
+		metadata : {
+			manifest: "json"
+		},
+
+		init : function () {
+			// call the init function of the parent
+			UIComponent.prototype.init.apply(this, arguments);
+
+			// set data model
+			var oData = {
+				recipient : {
+					name : "World"
+				}
+			};
+			var oModel = new JSONModel(oData);
+			this.setModel(oModel);
+        },
+        showHello: function() {
+            var oBundle = this.getView().getModel("i18n").getResourceBundle();
+			var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+			var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+			MessageToast.show(sMsg);
+        }
+
+	});
+
+});
